55af7136eb3fe7298e9d441bf11453ad306e044f
Esdras Battosti <esdrasbattostisilva@gmail.com>
2023-02-09 20:50:06 -0300

    refactor(NeuralNetwork): adding type hints

--- src/main.py
+++ src/main.py
@@ -1,19 +1,21 @@
 import numpy as np
+import numpy.typing as npt
+from layers import Dense, Sigmoid
 
 
 class NeuralNetwork:
-    def __init__(self, layers):
+    def __init__(self, layers: list[Dense | Sigmoid]) -> None:
         self.layers = layers
         self.n_layers = len(layers)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"NeuralNetwork(layers={self.layers})"
 
-    def initialize_parameters(self):
+    def initialize_parameters(self) -> None:
         for layer in self.layers:
             layer.initialize_parameters()
 
-    def forward_propagation(self, x_train):
+    def forward_propagation(self, x_train: npt.NDArray) -> npt.NDArray:
         A = x_train
 
         for layer in self.layers:
@@ -21,14 +23,14 @@ class NeuralNetwork:
 
         return A
 
-    def backward_propagation(self, y_train, learning_rate):
+    def backward_propagation(self, y_train: npt.NDArray, learning_rate: float) -> None:
         dA = y_train
 
         for layer in reversed(self.layers):
             dW, db, dA = layer.backward(dA)
             layer.update_parameters(learning_rate, dW, db)
 
-    def cost(self, predicted, y):
+    def cost(self, predicted: npt.NDArray, y: npt.NDArray) -> npt.NDArray:
         m = y.shape[1]
         epsilon = 1e-15
         predicted = np.clip(predicted, epsilon, 1-epsilon)
@@ -39,7 +41,14 @@ class NeuralNetwork:
 
         return np.squeeze(cost)
 
-    def fit(self, x_train, y_train, epochs, learning_rate, print_step=100):
+    def fit(
+        self,
+        x_train: npt.NDArray,
+        y_train: npt.NDArray,
+        epochs: int,
+        learning_rate: float,
+        print_step: int = 100,
+    ) -> None:
         self.initialize_parameters()
 
         for epoch in range(epochs):
@@ -50,13 +59,13 @@ class NeuralNetwork:
                 cost = self.cost(predicted, y_train)
                 print(f"Epoch: {epoch}, Cost: {cost}\n")
 
-    def predict(self, x_test):
+    def predict(self, x_test: npt.NDArray) -> npt.NDArray | np.float64:
         predicted = self.forward_propagation(x_test)
         predicted = np.round(predicted)
 
         return predicted
 
-    def evaluate(self, x_test, y_test):
+    def evaluate(self, x_test: npt.NDArray, y_test: npt.NDArray) -> np.float64:
         predictions = self.predict(x_test)
         correct_predictions = np.sum(predictions == y_test)
         accuracy = correct_predictions / y_test.shape[1]
