62762227e030bd29605b35a134c3e353f561dd57
Esdras Battosti <esdrasbattostisilva@gmail.com>
2023-02-09 21:20:37 -0300

    refactor(layers): adding type hints

--- src/layers.py
+++ src/layers.py
@@ -1,40 +1,43 @@
 import numpy as np
+import numpy.typing as npt
 
 
-def relu(Z):
+def relu(Z: npt.NDArray) -> npt.NDArray:
     return np.maximum(0, Z)
 
 
-def relu_prime(Z):
+def relu_prime(Z: npt.NDArray) -> npt.NDArray:
     return np.greater_equal(Z, 0).astype(int)
 
 
-def sigmoid(Z):
+def sigmoid(Z: npt.NDArray) -> npt.NDArray:
     return 1 / (1 + np.exp(-Z))
 
 
 class Dense:
-    def __init__(self, input_shape, output_shape):
+    def __init__(self, input_shape: int, output_shape: int) -> None:
         self.input_shape = input_shape
         self.output_shape = output_shape
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return (
             f"Dense(input_shape={self.input_shape}, output_shape={self.output_shape})"
         )
 
-    def initialize_parameters(self):
+    def initialize_parameters(self) -> None:
         self.weights = np.random.randn(self.output_shape, self.input_shape)
         self.bias = np.random.randn(self.output_shape, 1)
 
-    def forward(self, input):
+    def forward(self, input: npt.NDArray) -> npt.NDArray:
         self.input = input
         self.linear_output = np.dot(self.weights, self.input) + self.bias
         self.output = relu(self.linear_output)
 
         return self.output
 
-    def backward(self, dpartial):
+    def backward(
+        self, dpartial: npt.NDArray
+    ) -> tuple[npt.NDArray, npt.NDArray, npt.NDArray]:
         m = dpartial.shape[1]
 
         dZ = dpartial * relu_prime(self.linear_output)
@@ -44,33 +47,35 @@ class Dense:
 
         return dW, db, dA
 
-    def update_parameters(self, learning_rate, dW, db):
+    def update_parameters(
+        self, learning_rate: float, dW: npt.NDArray, db: npt.NDArray
+    ) -> None:
         self.weights = self.weights - learning_rate * dW
         self.bias = self.bias - learning_rate * db
 
 
 class Sigmoid:
-    def __init__(self, input_shape, output_shape):
+    def __init__(self, input_shape: int) -> None:
         self.input_shape = input_shape
         self.output_shape = output_shape
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return (
             f"Sigmoid(input_shape={self.input_shape}, output_shape={self.output_shape})"
         )
 
-    def initialize_parameters(self):
+    def initialize_parameters(self) -> None:
         self.weights = np.random.randn(self.output_shape, self.input_shape)
         self.bias = np.random.randn(self.output_shape, 1)
 
-    def forward(self, input):
+    def forward(self, input: npt.NDArray) -> npt.NDArray:
         self.input = input
         Z = np.dot(self.weights, self.input) + self.bias
         self.output = sigmoid(Z)
 
         return self.output
 
-    def backward(self, Y):
+    def backward(self, Y: npt.NDArray) -> tuple[npt.NDArray, npt.NDArray, npt.NDArray]:
         m = Y.shape[1]
 
         dZ = self.output - Y
@@ -80,6 +85,8 @@ class Sigmoid:
 
         return dW, db, dA
 
-    def update_parameters(self, learning_rate, dW, db):
+    def update_parameters(
+        self, learning_rate: float, dW: npt.NDArray, db: npt.NDArray
+    ) -> None:
         self.weights = self.weights - learning_rate * dW
         self.bias = self.bias - learning_rate * db
